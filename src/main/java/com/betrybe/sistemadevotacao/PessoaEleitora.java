a5f527e4d1cd0d44b145f8c1bef3d407fa193d1f
Gustavo-Oliveira7 <gustavo.h.oliveira7531@gmail.com>
2023-09-22 16:27:06 -0300

    PessoaEleitora implementada

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,33 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa {
 
+  private String cpf;
+
+  /**
+   * Cria classe Pessoa Eleitora, receberdo cpf.
+   */
+  public PessoaEleitora(String nome, String cpf) {
+    this.setNome(nome);
+    this.setCpf(cpf);
+
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  @Override
+  public void setNome(String nome) {
+    super.setNome(nome);
+  }
+
+  @Override
+  public String getNome() {
+    return super.getNome();
+  }
 }
