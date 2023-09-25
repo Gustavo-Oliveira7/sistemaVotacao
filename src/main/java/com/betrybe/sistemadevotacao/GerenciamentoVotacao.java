1030117ad75fd9821ee9cf81399319cd4c405cb0
Gustavo-Oliveira7 <gustavo.h.oliveira7531@gmail.com>
2023-09-25 12:45:58 -0300

    req5

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -19,12 +19,26 @@ public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
   @Override
   public void cadastrarPessoaCandidata(String nome, int numero) {
-
+    for (PessoaCandidata indexPessoaCandidata : pessoasCandidatas) {
+      if (indexPessoaCandidata.getNumero() == numero) {
+        System.out.println("Número da pessoa candidata já utilizado!");
+      } else {
+        PessoaCandidata pessoaCandidata = new PessoaCandidata(nome, numero);
+        pessoasCandidatas.add(pessoaCandidata);
+      }
+    }
   }
 
   @Override
   public void cadastrarPessoaEleitora(String nome, String cpf) {
-
+    for (PessoaEleitora indexPessoaEleitora : pessoasEleitoras) {
+      if (cpf.equals(indexPessoaEleitora.getCpf())) {
+        System.out.println("Pessoa eleitora já cadastrada!");
+      } else {
+        PessoaEleitora pessoaEleitora = new PessoaEleitora(nome, cpf);
+        pessoasEleitoras.add(pessoaEleitora);
+      }
+    }
   }
 
   @Override
