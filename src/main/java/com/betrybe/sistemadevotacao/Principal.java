4bda5181ef3b8ad64d0a525256a3a26f52fd5ebf
Gustavo-Oliveira7 <gustavo.h.oliveira7531@gmail.com>
2023-09-27 15:56:38 -0300

    classe principal desenvolvida

--- src/main/java/com/betrybe/sistemadevotacao/Principal.java
+++ src/main/java/com/betrybe/sistemadevotacao/Principal.java
@@ -1,10 +1,107 @@
 package com.betrybe.sistemadevotacao;
 
+import java.util.Scanner;
+
 public class Principal {
+  static GerenciamentoVotacao votacao = new GerenciamentoVotacao();
+
+  /**
+   * inicia a execução do programa.
+   */
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    ///menuVotacao();
 
+    cadastroPessoasCandidadatas();
+
+    ///cadastroPessoasEleitoras();
   }
 
+  /**
+   * cadastra pessoas candidatas.
+   */
+
+  public static void cadastroPessoasCandidadatas() {
+    System.out.println("Cadastrar Pessoa candidata?\n1 - Sim\n2 - Não\n"
+        + "Entre com o número correspondente à opção desejada:");
+    Scanner scanner = new Scanner(System.in);
+    int opcaoSelecionada = scanner.nextInt();
+
+    switch (opcaoSelecionada) {
+      case 1:
+        System.out.println("Entre com o nome da pessoa candidata:");
+        String nomeCandidata = scanner.next();
+        System.out.println("Entre com o número da pessoa candidata:");
+        int numeroCandidata = scanner.nextInt();
+        votacao.cadastrarPessoaCandidata(nomeCandidata, numeroCandidata);
+        cadastroPessoasCandidadatas();
+        break;
+      case 2:
+        cadastroPessoasEleitoras();
+        break;
+      default:
+        System.out.println("Opção inválida");
+        cadastroPessoasCandidadatas();
+    }
+  }
+
+  /**
+   * cadastra pessoas eleitoras.
+   */
+  public static void cadastroPessoasEleitoras() {
+    System.out.println("Cadastrar Pessoa eleitora?\n1 - Sim\n2 - Não\n"
+        + "Entre com o número correspondente à opção desejada:");
+    Scanner scanner = new Scanner(System.in);
+    int opcaoSelecionada = scanner.nextInt();
+    switch (opcaoSelecionada) {
+      case 1:
+        System.out.println("Entre com o nome da pessoa eleitora:");
+        String nomeEleitora = scanner.next();
+        System.out.println("Entre com o cpf da pessoa eleitora:");
+        String cpfEleitora = scanner.next();
+        votacao.cadastrarPessoaEleitora(nomeEleitora, cpfEleitora);
+        cadastroPessoasEleitoras();
+        break;
+
+      case 2:
+        menuVotacao();
+        break;
+
+      default:
+        System.out.println("Opção inválida");
+        cadastroPessoasEleitoras();
+
+    }
+  }
+
+  /**
+   * Abre o menu votação.
+   */
+  public static void menuVotacao() {
+    System.out.println("Entre com o número correspondente à opção desejada:\n1 - Votar\n"
+        + "2 - Resultado Parcial\n"
+        + "3 - Finalizar Votação");
+    Scanner scanner = new Scanner(System.in);
+    int opcaoSelecionada = scanner.nextInt();
+    switch (opcaoSelecionada) {
+      case 1:
+        System.out.println("Entre com o cpf da pessoa eleitora");
+        String cpfEleitora = scanner.next();
+        System.out.println("Entre com o número da pessoa candidata:");
+        int numeroCandidato = scanner.nextInt();
+        votacao.votar(cpfEleitora, numeroCandidato);
+        menuVotacao();
+        break;
+      case 2:
+        votacao.mostrarResultado();
+        break;
+      case 3:
+        votacao.mostrarResultado();
+        menuVotacao();
+        break;
+      default:
+        System.out.println("Opção inválida");
+        menuVotacao();
+    }
+  }
 }
